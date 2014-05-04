dc8a77f17cc001461177914e5a5d44e831f72b1a
dorahagen <dhagen@dooleyandassociates.com>
2014-05-04 08:03:50 -0500

    First part of jQuery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
 
+	// place cursor in input area
+	$('#entry').focus();
+
+	//enter item name in input area
+	function addItem() {
+		var item = $('#entry').val();
+		if(!$.trim($('#entry').val())) {
+            alert('You forgot to enter your item');
+        } else {
+			$("<li class='unchecked'></li>").appendTo('#list').html(item+"<div class='remove'></div>");
+        }
+        $('#entry').val(''); // clear input area
+	}
+	// add new item to bottom of list
+	$('.add').click(addItem);
+
+	// make enter submit button
+	$('#entry').keyup(function(event) {
+		if(event.keyCode == 13){
+			$('.add').click();
+		}
+	});
+
+	// delete item when x is clicked
+	$('#list').on('click','.remove', function() {
+		$(this).closest('li').remove();
+	});
+
+	//cross off items
+	$('#list').on('click','li', function() {
+		$(this).toggleClass('.checked');
+	});
+
 });
 
